dfb5f53304c4c561afa2828b6a744e013ad2d1dd
Игорь <dragomerogod@gmail.com>
2025-01-24 20:04:32 +0200

    KAN-8: Исправлен баг передвижения

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.InputSystem.XR.Haptics;
 
 public class Move : MonoBehaviour
 {
@@ -7,19 +6,48 @@ public class Move : MonoBehaviour
     private Vector2 direction;
     private Rigidbody2D rb;
 
-    void Start ()
+    void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
-    void Update ()
+    void Update()
     {
-        direction.x = Input.GetAxisRaw("Horizontal");
-        direction.y = Input.GetAxisRaw("Vertical");
-    }    
+        direction = Vector2.zero;   //обнуление вектора движения
 
-    void FixedUpdate ()
+        //горизонтальный вектор
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            direction.x = -1;                                                      //движение влево
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            direction.x = 1;                                                       //движение вправо
+        }
+
+        //вертикальный вектор
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            direction.y = 1;                                                       //движение вверх
+        }
+        else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            direction.y = -1;                                                      //движение вниз
+        }
+
+        //препятствие диагональному передвижению
+        
+        if (direction.x != 0)
+        {
+            direction.y = 0;                                                       //обнуление вертикального вектора
+        }
+        else if (direction.y != 0)
+        {
+            direction.x = 0;                                                       //обнуление горизонтального вектора
+        }
+    }
+
+    void FixedUpdate()
     {
         rb.MovePosition(rb.position + direction * speed * Time.fixedDeltaTime);
     }
-
 }
